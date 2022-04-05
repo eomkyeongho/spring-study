de7c777d602f4e18ffacb435a3c277267d903515
ekh <52539130+eomkyeongho@users.noreply.github.com>
2022-04-06 01:34:49 +0900

    spring 기본 사용법

--- hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -1,13 +1,13 @@
-package hello.core;
+package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreApplication
+public class HelloSpringApplication
 {
 	public static void main(String[] args)
 	{
-		SpringApplication.run(CoreApplication.class, args);
+		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 }
