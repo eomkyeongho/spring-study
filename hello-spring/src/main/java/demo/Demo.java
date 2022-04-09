42301477a78dedfe8e920c40580efa6ed0cedd59
ekh <52539130+eomkyeongho@users.noreply.github.com>
2022-04-09 23:18:49 +0900

    스프링빈  + DI

---
+++ hello-spring/src/main/java/demo/Demo.java
@@ -0,0 +1,7 @@
+package demo;
+
+import org.springframework.stereotype.Service;
+
+@Service // hellospring 스프링빈에 등록 안됨 -> 같은 level의 디렉토리여서
+public class Demo {
+}
--- hello-spring/src/main/java/hello/hellospring/Controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/Controller/HelloController.java
@@ -7,40 +7,34 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController
-{
+public class HelloController {
     @GetMapping("hello")
-    public String hello(Model model)
-    {
+    public String hello(Model model) {
         model.addAttribute("data","its me!!");
         return "hello";
     }
 
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam(value = "name") String name, Model model)
-    {
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
         model.addAttribute("name",name);
         return "hello-template";
     }
 
     @GetMapping("hello-string")
     @ResponseBody
-    public String helloString(@RequestParam("name") String name)
-    {
+    public String helloString(@RequestParam("name") String name) {
         return "hello " + name;
     }
 
     @GetMapping("hello-api")
     @ResponseBody
-    public Hello helloApi(@RequestParam("name") String name)
-    {
+    public Hello helloApi(@RequestParam("name") String name) {
         Hello hello = new Hello();
         hello.setName(name);
         return hello;
     }
 
-    static class Hello
-    {
+    static class Hello {
         private String name;
 
         public String getName()
