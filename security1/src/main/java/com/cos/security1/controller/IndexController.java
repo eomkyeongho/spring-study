e2360aa4621cda6d06f5c7fcc974eae035692896
ekh <52539130+eomkyeongho@users.noreply.github.com>
2023-06-26 01:29:41 +0900

    spring security example

---
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -0,0 +1,87 @@
+package com.cos.security1.controller;
+
+import com.cos.security1.domain.User;
+import com.cos.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping({"", "/"})
+    public String index() {
+        return "index";
+    }
+
+    @ResponseBody
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @ResponseBody
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @ResponseBody
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/loginForm")
+    public String login() {
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
+    }
+
+    @ResponseBody
+    @GetMapping("/joinProc")
+    public String joinProc() {
+        return "회원가입 완료됨!";
+    }
+
+    @PostMapping("/join")
+    public String join(User user) {
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+
+        userRepository.save(user);
+
+        return "redirect:/loginForm";
+    }
+
+    @ResponseBody
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public String info() {
+        return "info";
+    }
+
+    @ResponseBody
+    @GetMapping("/data")
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+    public String data() {
+        return "data";
+    }
+}
---
+++ security1/src/main/java/com/cos/security1/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.cos.security1.repository;
+
+import com.cos.security1.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    public User findByUsername(String username);
+    public User findByEmail(String email);
+}
